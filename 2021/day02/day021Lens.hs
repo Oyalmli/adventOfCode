0aa179db234c8622996dd96600cb7238d2262207
Oyalmli <oyvind.almli@gmail.com>
2021-12-02 11:25:01 +0100

    Formatting Changes to the Lens solution

--- 2021/day02/day021Lens.hs
+++ 2021/day02/day021Lens.hs
@@ -11,17 +11,14 @@ makeLenses ''Sub
 main :: IO ()
 main = interact
   $ show
-  . solve (Sub {_hor=0, _dpt=0})
+  . flip solve (Sub {_hor=0, _dpt=0})
   . lines
 
-solve :: Sub -> [String] -> Int
-solve sub []        = _hor sub * _dpt sub
-solve sub (l : ls)  = solve ( 
-  case dir of
-    "forward" -> sub & hor +~ n
-    "down"    -> sub & dpt +~ n
-    "up"      -> sub & dpt -~ n
-  ) ls
-  where 
-    [dir, sn] = words l
-    n = read sn
+solve :: [String] -> Sub -> Int
+solve []       sub   = _hor sub * _dpt sub
+solve (l : ls) sub   = solve ls 
+  ( case dir of
+    "forward" -> sub &hor +~ n
+    "down"    -> sub &dpt +~ n
+    "up"      -> sub &dpt -~ n )
+  where [dir, sn] = words l; n = read sn
--- 2021/day02/day022Lens.hs
+++ 2021/day02/day022Lens.hs
@@ -12,20 +12,14 @@ makeLenses ''Sub
 main :: IO ()
 main = interact 
   $ show
-  . solve (Sub {_hor = 0, _dpt = 0, _aim = 0})
+  . flip solve (Sub {_hor = 0, _dpt = 0, _aim = 0})
   . lines
 
-solve :: Sub -> [String] -> Int
-solve sub [] = _hor sub * _dpt sub
-solve sub (l : ls) = solve ( 
-  case dir of
-    "up"      -> sub & aim -~ n
-    "down"    -> sub & aim +~ n
-    "forward" ->
-      sub
-        & hor +~ n
-        & dpt +~ n * _aim sub
-    ) ls
-  where
-    [dir, sn] = words l
-    n = read sn
+solve :: [String] -> Sub -> Int
+solve []       sub = _hor sub * _dpt sub
+solve (l : ls) sub = solve ls 
+  ( case dir of
+    "up"      -> sub &aim -~ n
+    "down"    -> sub &aim +~ n
+    "forward" -> sub &hor +~ n &dpt +~ n * _aim sub )
+  where [dir, sn] = words l; n = read sn
