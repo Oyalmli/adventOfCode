1bc815b0921eb05aa3758865125fa9639a3852a3
Oyalmli <oyvind.almli@gmail.com>
2021-12-02 11:53:50 +0100

    Formatting changes day 2

--- 2021/day02/day021.hs
+++ 2021/day02/day021.hs
@@ -3,15 +3,14 @@ module Main where
 main :: IO ()
 main = interact
   $ show
-  . solve (0, 0)
+  . flip solve (0, 0)
   . lines
 
-solve :: (Int, Int) -> [String] -> Int
-solve (hori, depth) []        = hori * depth
-solve (hori, depth) (l : ls)  = solve ( 
-  case dir of
+solve :: [String] -> (Int, Int) ->  Int
+solve []       (hori, depth) = hori * depth
+solve (l : ls) (hori, depth) = solve ls
+  ( case dir of
     "forward" -> (hori + read sn,           depth)
     "down"    -> (hori          , depth + read sn)
-    "up"      -> (hori          , depth - read sn)
-  ) ls
+    "up"      -> (hori          , depth - read sn) ) 
   where [dir, sn] = words l
--- 2021/day02/day022.hs
+++ 2021/day02/day022.hs
@@ -3,17 +3,14 @@ module Main where
 main :: IO ()
 main = interact
   $ show
-  . solve (0, 0, 0)
+  . flip solve (0, 0, 0)
   . lines
 
-solve :: (Int, Int, Int) -> [String] -> Int
-solve (hori, depth, aim) []       = hori * depth
-solve (hori, depth, aim) (l : ls) = solve ( 
-  case dir of
+solve :: [String] -> (Int, Int, Int) ->  Int
+solve []       (hori, depth, aim) = hori * depth
+solve (l : ls) (hori, depth, aim) = solve ls 
+  ( case dir of
     "forward" -> (hori+n, depth + n*aim, aim    )
     "down"    -> (hori  , depth        , aim + n)
-    "up"      -> (hori  , depth        , aim - n)
-  ) ls
-  where 
-    [dir, sn] = words l
-    n = read sn
+    "up"      -> (hori  , depth        , aim - n) )
+  where [dir, sn] = words l; n = read sn
