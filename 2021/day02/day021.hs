63ceb1977305e7d8ef677b0fe80e9310b5c871fb
Oyalmli <oyvind.almli@gmail.com>
2021-12-02 12:07:55 +0100

    Now using foldl

--- 2021/day02/day021.hs
+++ 2021/day02/day021.hs
@@ -3,14 +3,16 @@ module Main where
 main :: IO ()
 main = interact
   $ show
-  . flip solve (0, 0)
+  . uncurry (*)
+  . foldl solve (0, 0)
   . lines
 
-solve :: [String] -> (Int, Int) ->  Int
-solve []       (hor, dpt) = hor * dpt
-solve (l : ls) (hor, dpt) = solve ls
-  ( case dir of
+solve :: (Int, Int) -> String -> (Int, Int)
+solve (hor, dpt) l = 
+  case dir of
     "forward" -> (hor+n, dpt  )
     "down"    -> (hor  , dpt+n)
-    "up"      -> (hor  , dpt-n) ) 
-  where [dir, sn] = words l; n = read sn
+    "up"      -> (hor  , dpt-n)
+  where 
+    [dir, sn] = words l
+    n = read sn
--- 2021/day02/day022.hs
+++ 2021/day02/day022.hs
@@ -3,14 +3,16 @@ module Main where
 main :: IO ()
 main = interact
   $ show
-  . flip solve (0, 0, 0)
+  . (\(hor, dep,_) -> hor * dep)
+  . foldl solve (0, 0, 0)
   . lines
 
-solve :: [String] -> (Int, Int, Int) ->  Int
-solve []       (hor, dpt, aim) = hor * dpt
-solve (l : ls) (hor, dpt, aim) = solve ls 
-  ( case dir of
+solve :: (Int, Int, Int) -> String -> (Int, Int, Int)
+solve (hor, dpt, aim) l =
+  case dir of
     "forward" -> (hor+n, dpt + n*aim, aim  )
     "down"    -> (hor  , dpt        , aim+n)
-    "up"      -> (hor  , dpt        , aim-n) )
-  where [dir, sn] = words l; n = read sn
+    "up"      -> (hor  , dpt        , aim-n)
+  where 
+    [dir, sn] = words l
+    n = read sn
