3969a84b204d282cc24ad3dc1770ec111633080c
Oyalmli <oyvind.almli@gmail.com>
2021-12-03 12:37:20 +0100

    pulled function out into function

--- 2021/day03/day031.hs
+++ 2021/day03/day031.hs
@@ -7,8 +7,11 @@ import Control.Monad ( liftM2 )
 main :: IO ()
 main = interact 
   $ show . solve
-  . map (liftM2 (,) sum (liftM2 (-) length sum) . map digitToInt)
+  . map (onesAndZeroes . map digitToInt)
   . transpose . lines
+
+onesAndZeroes :: [Int] -> (Int, Int)
+onesAndZeroes = liftM2 (,) sum (liftM2 (-) length sum)
 
 solve :: [(Int, Int)] -> Int
 solve = ((*) . f (>)) <*> f (<)
