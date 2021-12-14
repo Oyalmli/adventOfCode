c72ffc5dc21537fbd8fdf2f57e6abeae20fff738
Oyalmli <oyvind.almli@gmail.com>
2021-12-14 13:42:47 +0100

    Python solution now solved p1 and p2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -15,21 +15,24 @@ def countChars(hist):
     res = [v for (_,v) in res.items()]
     return max(res) - min(res)
 
-def main():
-    inp = [line.strip() for line in sys.stdin.readlines()]
-    pHist = Counter(zip(inp[0], inp[0][1:]))
-    ruleMap = makeRuleMap(inp[2:])
-    
-    for _ in range(40):
+def solve(n, ruleMap, pHist):
+    for _ in range(n):
         npHist = pHist.copy()
         for (a,b), v in pHist.items():
             c = ruleMap[(a,b)]
             npHist[(a,b)] -= v
             npHist[(a,c)] += v
             npHist[(c,b)] += v
-        pHist = npHist
+        pHist = npHist.copy()
+    return countChars(pHist)
+
+def main():
+    inp = [line.strip() for line in sys.stdin.readlines()]
+    pHist = Counter(zip(inp[0], inp[0][1:]))
+    ruleMap = makeRuleMap(inp[2:])
     
-    print(countChars(pHist))
+    print("Part 1:",solve(10, ruleMap, pHist))
+    print("Part 2:",solve(40, ruleMap, pHist))
 
 if __name__ == "__main__":
     main()
